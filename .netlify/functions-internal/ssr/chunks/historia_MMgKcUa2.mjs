0eea4d996794306a3f0fd503df4baa13e05fbb49
NotSleepp <danielfranconot@gmail.com>
2024-02-21 15:26:40 -0300

    buil

---
+++ .netlify/functions-internal/ssr/chunks/historia_MMgKcUa2.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/historia_7UeSzVxH.mjs').then(n => n.h);
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/pages/inicio_pLKwTfg7.mjs
@@ -0,0 +1,75 @@
+/* empty css                             */
+import { e as createAstro, f as createComponent, r as renderTemplate, m as maybeRenderHead, j as renderComponent } from '../astro_VTOZsQXq.mjs';
+import 'kleur/colors';
+import 'html-escaper';
+import 'clsx';
+import 'cssesc';
+import { $ as $$Layout } from './historia_7UeSzVxH.mjs';
+/* empty css                           */
+
+const $$Astro$6 = createAstro();
+const $$FlorDePie = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$6, $$props, $$slots);
+  Astro2.self = $$FlorDePie;
+  return renderTemplate`${maybeRenderHead()}<div data-astro-cid-or2akebi> <img class="florDePie" src="../../../pngegg2.png" alt="" data-astro-cid-or2akebi> </div> `;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/components/imagenes/FlorDePie.astro", void 0);
+
+const $$Astro$5 = createAstro();
+const $$FlorDePie2 = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$5, $$props, $$slots);
+  Astro2.self = $$FlorDePie2;
+  return renderTemplate`${maybeRenderHead()}<div data-astro-cid-xt6mttkc> <img class="florDePie" src="../../../pngegg2copia.png" alt="" data-astro-cid-xt6mttkc> </div> `;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/components/imagenes/FlorDePie2.astro", void 0);
+
+const $$Astro$4 = createAstro();
+const $$GaleriaInicio = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$4, $$props, $$slots);
+  Astro2.self = $$GaleriaInicio;
+  return renderTemplate`${maybeRenderHead()}<section class="mt-64"> ${renderComponent($$result, "FlorDePie2", $$FlorDePie2, {})} ${renderComponent($$result, "FlorDePie", $$FlorDePie, {})} <div class=" vermas columns-4 gap-3 w-[1200px] mx-auto space-y-3 pb-28 grid grid-cols-2 md:grid-cols-4 p-16 bg-white/30 rounded-3xl"> <div class="grid gap-4"> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen2.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen3.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen4.jpg" alt=""> </div> </div> <div class="grid gap-4"> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen5.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen6.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen7.jpg" alt=""> </div> </div> <div class="grid gap-4"> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen8.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen9.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen10.jpg" alt=""> </div> </div> <div class="grid gap-4"> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen11.jpg" alt=""> </div> <div> <img class="h-auto max-w-full rounded-lg" src="./public/images/imagen12.jpg" alt=""> </div> </div> </div> </section>`;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/components/GaleriaInicio.astro", void 0);
+
+const $$Astro$3 = createAstro();
+const $$FloresTitulo = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$3, $$props, $$slots);
+  Astro2.self = $$FloresTitulo;
+  return renderTemplate`${maybeRenderHead()}<div data-astro-cid-wq5g56y3> <img id="floresTitulo" src="../../../pngegg1.png" alt="" data-astro-cid-wq5g56y3> </div> `;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/components/imagenes/FloresTitulo.astro", void 0);
+
+const $$Astro$2 = createAstro();
+const $$BraniHoraDeAventura = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$2, $$props, $$slots);
+  Astro2.self = $$BraniHoraDeAventura;
+  return renderTemplate`${maybeRenderHead()}<div data-astro-cid-4mlstoxq> <img class="BraniHoraDeAventura" src="../../../brani.png" alt="" data-astro-cid-4mlstoxq> </div> `;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/components/imagenes/BraniHoraDeAventura.astro", void 0);
+
+const $$Astro$1 = createAstro();
+const $$FlorEsquina = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
+  Astro2.self = $$FlorEsquina;
+  return renderTemplate`${maybeRenderHead()}<div data-astro-cid-dxzsd6gg> <img class="florEsquina" src="../../../florEsquina.png" alt="" data-astro-cid-dxzsd6gg> </div> `;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/components/imagenes/FlorEsquina.astro", void 0);
+
+const $$Astro = createAstro();
+const $$Inicio = createComponent(async ($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Inicio;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Inicio" }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<main> ${renderComponent($$result2, "FlorEsquina", $$FlorEsquina, {})} ${renderComponent($$result2, "BraniHoraDeAventura", $$BraniHoraDeAventura, {})} <section class="w-full mx-auto lg:w-[740px] mt-[184px] mb-[122px] bg-white/50 p-16"> <h1 class="text-5xl font-bold flex flex-row gap-x-4 pb-6 text-black">
+Bren & Dani
+</h1> <p class="text-xl opacity-80 text-wrap">
+Hola mi amor, ¡Bienvenida a este espacio que cree para festejar nuestro
+        aniversario! este lugar lo cree especialmente para ver y revivir nuestra
+        historia, compartir nuevos momentos y soñar con un futuro juntos. vas a
+        poder ver nuestra historia desde mi perspectiva, desde el primer momento
+        que nos vimos. vas a ver también fotos nuestras, música que nos
+        representen o que nos dedicamos y seguramente en algún futuro vamos a
+        tener juegos y más. pero no es solo un álbum de recuerdos bebe, va a
+        irse actualizando! va a ser un espacio donde podamos seguir escribiendo
+        toda nuestra historia y compartir experiencias nuevas espero que te
+        guste este espacio que cree con todo mi amor para vos.
+</p> ${renderComponent($$result2, "FloresTitulo", $$FloresTitulo, {})} </section> ${renderComponent($$result2, "GaleriaInicio", $$GaleriaInicio, {})} </main> ` })}`;
+}, "C:/Users/danie/Desktop/proyect a\xF1o/brenax/src/pages/inicio.astro", void 0);
+
+const $$file = "C:/Users/danie/Desktop/proyect año/brenax/src/pages/inicio.astro";
+const $$url = "/inicio";
+
+export { $$Inicio as default, $$file as file, $$url as url };
