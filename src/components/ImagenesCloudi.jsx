f945fe411ed72a45c6f4e25807935b90e1b6f936
NotSleepp <danielfranconot@gmail.com>
2024-03-14 18:51:50 -0300

    aa

---
+++ src/components/ImagenesCloudi.jsx
@@ -0,0 +1,17 @@
+import { Container } from 'reactstrap';
+import Dropzone from 'react-dropzone';
+import {useState} from 'react';
+
+const ImagenesCloudi = (props) => {
+    const [image, setImage] = useState()
+
+  return (
+    <div className='mt-64'>
+      <Container>
+      
+      </Container>
+    </div>
+  )
+}
+
+export default ImagenesCloudi
